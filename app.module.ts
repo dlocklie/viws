061219a7e461648c3c35af6f1d73ecf0ebf003da
Drew Lockliear <dlockliear@gmail.com>
2020-08-06 19:13:57 -0500

    Angular router working

--- app.module.ts
+++ app.module.ts
@@ -12,12 +12,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BodyComponent,
-    MainNavComponent
+    MainNavComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
